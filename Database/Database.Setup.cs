722a3e7a270aab1b0fc08f652b8e672bb43140aa
lucaxlean <lucas.paula@aluno.ifsp.edu.br>
2022-05-18 08:35:54 -0300

    refactor: add database setup class

---
+++ Database/Database.Setup.cs
@@ -0,0 +1,30 @@
+using Microsoft.Data.Sqlite;
+
+namespace LabManager.Database; 
+
+
+class DatabaseSetup
+{
+    public DatabaseSetup()
+    {
+        CreateComputerTable();
+    } 
+
+        public void CreateComputerTable()
+    {
+        var connection = new SqliteConnection("Data Source=database.db"); 
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Computers(
+                id int not null primary key,
+                ram varchar(100) not null,
+                processor varchar(100) not null
+            );
+        ";
+
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
